07a51384a6330874519fc527e15316c30bc8b31c
Nya Garcia <nyablk97@gmail.com>
2019-10-02 13:54:01 +0200

    refactor(signup.component): update validateForm import path

--- src/components/signup/signup.component.tsx
+++ src/components/signup/signup.component.tsx
@@ -6,7 +6,7 @@ import { TextField } from "final-form-material-ui";
 import Button from "@material-ui/core/Button";
 import { Grid } from "@material-ui/core";
 import { Form, Field } from "react-final-form";
-import { validateForm } from "services/index";
+import { validateForm } from "pods/signup/index";
 
 export const SignupComponent = (): JSX.Element => {
 
