7ac0f77c9eab5604b6a690094f40767eaa843fec
Nya Garcia <nyablk97@gmail.com>
2019-10-02 13:54:34 +0200

    feat(main.constants): add VALID_GITHUB_IDS constant

--- src/constants/main.constants.ts
+++ src/constants/main.constants.ts
@@ -2,6 +2,8 @@ export const PASSWORD_REGEXP = /^(?=.{5,}$)(?=.*?[a-zA-Z\d])(?=.*?[\d])(?=.*?[#_
 
 export const GITHUB_ENDPOINT = "https://api.github.com/users";
 
+export const VALID_GITHUB_IDS = ['NyaGarcia', 'username'];
+
 export const VALIDATOR_TYPES = {
   MATCH: {
     TYPE: "MATCH_FIELD_VALIDATOR",
