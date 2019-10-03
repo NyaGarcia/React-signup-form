cc6c00c29e717b176901eb88a750c423ffe496a7
Nya Garcia <nyablk97@gmail.com>
2019-10-03 18:14:30 +0200

    feat(github.service.spec): add two more test cases

--- __tests__/github.service.spec.ts
+++ __tests__/github.service.spec.ts
@@ -1,22 +1,23 @@
-import { isGithubAccount } from 'pods/signup/github.service';
 import { GITHUB_ENDPOINT } from 'constants/main.constants';
+import { isGithubAccount } from 'pods/signup/github.service';
 
 describe('isGithubAccount function tests', () => {
   beforeEach(() => {
     fetchMock.resetMocks();
-  })
+  });
   it.each`
-   username         | result
-  ${'NyaGarcia'}   | ${true}
-  ${'username'}    | ${true}
-  ${'unknown'}     | ${false}
-  ${''}            | ${false}
-  ${0}             | ${false}
+    username       | result
+    ${'NyaGarcia'} | ${true}
+    ${'username'}  | ${true}
+    ${'unknown'}   | ${false}
+    ${''}          | ${false}
+    ${0}           | ${false}
+    ${undefined}   | ${false}
+    ${null}        | ${false}
   `('Should return Promise<$result> with $username value', async () => {
-    fetchMock.mockResponseOnce(JSON.stringify({login: '$result'}));
+    fetchMock.mockResponseOnce(JSON.stringify({ login: '$result' }));
 
     const response = await isGithubAccount(`${GITHUB_ENDPOINT}/$result`);
     expect(response).toEqual('$result');
-  })
-})
-   
+  });
+});
