7460d657680bb5d3c6f5e08f1a38097d62a97915
Nya Garcia <nyablk97@gmail.com>
2019-10-04 11:54:25 +0200

    feat(setupJest): add enzyme config

--- config/test/setupJest.ts
+++ config/test/setupJest.ts
@@ -1,5 +1,9 @@
-import { GlobalWithFetchMock } from "jest-fetch-mock";
+import { Adapter } from 'enzyme-adapter-react-16';
+import { GlobalWithFetchMock } from 'jest-fetch-mock';
+import enzyme from 'enzyme';
 
 const customGlobal: GlobalWithFetchMock = global as GlobalWithFetchMock;
 customGlobal.fetch = require('jest-fetch-mock');
 customGlobal.fetchMock = customGlobal.fetch;
+
+enzyme.configure({ adapter: new Adapter() });
