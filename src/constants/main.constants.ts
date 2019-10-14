43645204130bbb59cff0d4ffe7434e4a64227433
Nya Garcia <nyablk97@gmail.com>
2019-10-14 14:15:42 +0200

    feat(main.constants): add earlier_date constant

--- src/constants/main.constants.ts
+++ src/constants/main.constants.ts
@@ -1,15 +1,20 @@
 export const PASSWORD_REGEXP = /^(?=.{5,}$)(?=.*?[a-zA-Z\d])(?=.*?[\d])(?=.*?[#_@$-])/;
 
-export const GITHUB_ENDPOINT = "https://api.github.com/users";
+export const GITHUB_ENDPOINT = 'https://api.github.com/users';
 
 export const VALID_GITHUB_IDS = ['NyaGarcia', 'username'];
 
 export const VALIDATOR_TYPES = {
   MATCH: {
-    TYPE: "MATCH_FIELD_VALIDATOR",
-    MESSAGE: "Fields do not match"},
-    GITHUB_ID: {
-      TYPE: "GITHUB_ID_VALIDATOR",
-      MESSAGE: "Not a valid GitHub ID"
-    }
-}
+    TYPE: 'MATCH_FIELD_VALIDATOR',
+    MESSAGE: 'Fields do not match',
+  },
+  GITHUB_ID: {
+    TYPE: 'GITHUB_ID_VALIDATOR',
+    MESSAGE: 'Not a valid GitHub ID',
+  },
+  EARLIER_DATE: {
+    TYPE: 'EARLIER_DATE_VALIDATOR',
+    MESSAGE: 'Not an earlier date',
+  },
+};
